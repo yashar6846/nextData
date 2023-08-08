3729baf342254e987f42f2847cda4d6a9e4c6c5a
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-08-08 14:25:54 +0300

    add files and instalspages

---
+++ my-app/components/EditTopicForm.jsx
@@ -0,0 +1,13 @@
+
+export default function EditTopicForm() {
+  console.log('jjjk');
+  return (
+    <form className="flex flex-col gap-3">
+    <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
+
+    <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
+
+    <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">UpdatTopic</button>
+</form>
+  )
+}
---
+++ my-app/components/TopicsList.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
+
+
+export default function TopicsList() {
+  return (
+    <>
+    <div  className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+       <div>
+        <h1>Topic Title</h1>
+        <div>Topic Description</div>
+    </div>
+    
+    <div className="flex gap-2">
+        <RemoveBtn />
+        <Link href={'/editTopic/123'}>
+           <HiPencilAlt size={24} />
+        </Link>
+    </div>
+    </div>
+    </>
+  )
+}
