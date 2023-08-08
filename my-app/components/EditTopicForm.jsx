ed3cddac0b433fd6929941df16ecc9788ee2cf37
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-08-08 18:23:45 +0300

    add resapi D Put P Get

--- my-app/components/EditTopicForm.jsx
+++ my-app/components/EditTopicForm.jsx
@@ -1,6 +1,6 @@
 
 export default function EditTopicForm() {
-  console.log('jjjk');
+
   return (
     <form className="flex flex-col gap-3">
     <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
