93254a409e9850ff74eef68e92c95b4fe760ecb9
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-08-09 15:27:35 +0300

    kheyle dhanam serves shod

--- my-app/app/addTopic/page.jsx
+++ my-app/app/addTopic/page.jsx
@@ -1,42 +1,65 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AddTopic() {
-  const [title, setTitle]= useState('')
-  const [description, setDescription]= useState('')
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-   const router = useRouter()
-  const handelSubmit= async(e)=>{
-   e.preventDefault();
-  
-   if(!title || !description){
-    alert('Title and description are required.')
-    return
-   }
-   try{
-   const res = await fetch(`http://localhost:3001/api/topics`,{
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({title, description})
-   })
-   if(res.ok){
-    router.push('/')
-   }else{
-    throw new Error("failed to connect a topic")
-   }
-   }catch(error){console.log(error);}
-  }
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and description are required.");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3001/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form onClick={handelSubmit} className="flex flex-col gap-3">
-        <input onChange={(e)=> setTitle(e.target.value)} value={title} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
 
-        <input onChange={(e)=> setDescription(e.target.value)} value={description}  className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
 
-        <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">Add Topic</button>
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Add Topic
+      </button>
     </form>
-  )
+  );
 }
--- my-app/components/RemoveBtn.jsx
+++ my-app/components/RemoveBtn.jsx
@@ -1,9 +1,25 @@
+"use client"
+import { useRouter } from "next/navigation";
 import { HiOutlineTrash } from "react-icons/hi";
 
+export default function RemoveBtn({id}) {
+  const router = useRouter()
+  const removeTopics = async (req, res, next) => {
+    const confirmed = confirm("Are you sure you want to remove");
 
-export default function RemoveBtn() {
+    if(confirmed) {
+     const res = await fetch(`http://localhost:3001/api/topics?id=${id}`,{
+        method: "DELETE",
+      });
+
+      if(res.ok){
+        router.refresh()
+      }
+      
+    }
+  }
   return (
-    <button className="text-red-400">
+    <button onClick={removeTopics} className="text-red-400">
         <HiOutlineTrash size={24} />
         </button>
   )
